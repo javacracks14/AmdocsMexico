022051c60d7810204986db3e4eac8dcdd9e43db3
Jean Carlos Carrazco Santos <jeancarrazco33@gmail.com>
2025-08-20 04:11:05 +0530

    Create OperatorsDemo.java

---
+++ Jean Carrazco/OperatorsDemo.java
@@ -0,0 +1,57 @@
+package com.amdocs;
+
+//public class OperatorsDemo {
+//public static void main(String[] args) {
+//	int number1 = 10, number2 =8, output;
+//	output = number1 + number2;
+//	System.out.println("Addition output is: " + output);
+//	output = number1 - number2;
+//	System.out.println("Addition output is: " + output);
+//	output = number1 * number2;
+//	System.out.println("Addition output is: " + output);
+//	output = number1 / number2;
+//	System.out.println("Addition output is: " + output);
+//	output = number1 % number2;
+//	System.out.println("Addition output is: " + output);
+//	number1 += number2;
+//	//number1 = number1+ number2
+//	System.out.println("Number 1 value is: " + number1 +  " Number 2 value is: "+number2);
+//	number1 -= number2;
+//	System.out.println("Number 1 value is: " + number1 + " Number 2 value is: "+number2);
+//	number1 *= number2;
+//	System.out.println("Number 1 value is: " + number1 + " Number 2 value is: "+number2);
+//	number1 /= number2;
+//	System.out.println("Number 1 value is: " + number1 + " Number 2 value is: "+number2);
+//}
+//}
+//public class OperatorsDemo {
+//public static void main(String[] args) {
+//	int number1 =10, number2 = 8;
+//	System.out.println("For greather than: " + (number1 > number2));
+//	System.out.println("For less than: " + (number1 < number2));
+//	
+//}
+//}
+//public class OperatorsDemo {
+//public static void main(String[] args) {
+//	int number1 =10, number2 = 8, number3=8;
+//	System.out.println("Logical and operator value: " + (number1 > number2 && number1 == number3));
+//	System.out.println("Logical or operator value: " + (number1 > number2 || number1 == number3));
+//	System.out.println("Logical and operator value: " + (number1 > number2 && number1 != number3));
+//}
+//}
+//public class OperatorsDemo {
+//public static void main(String[] args) {
+//	int number1 = 10;
+//	System.out.println("Value of number: "+number1);
+//	System.out.println("Value increment operation: "+(number1 ++));
+//	System.out.println("Value after: "+number1);
+//}
+//}
+public class OperatorsDemo {
+public static void main(String[] args) {
+	int number1 = 10, number2 =8;
+	System.out.println(number1>number2?"Number1 is greater":"Number 2 is greater");
+								       //first condition is true after : is false
+}
+}
