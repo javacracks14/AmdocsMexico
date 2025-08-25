ffc75c7949fccbe46ce9fdf489f6e55a84f0fc4d
Jean Carlos Carrazco Santos <jeancarrazco33@gmail.com>
2025-08-26 00:16:54 +0530

    Create Calculator.java

---
+++ Jean Carrazco/Calculator.java
@@ -0,0 +1,55 @@
+package com.amdocs;
+//
+//public class Calculator {
+//	public void add(int a, int b) {
+//		System.out.println("Adding (int, int):"+ (a+b));
+//	}
+//	public void add(int a, float b) {
+//		System.out.println("Adding (int, int):"+ (a+b));
+//	}
+//	public void add(float a, int b) {
+//		System.out.println("Adding (int, int):"+ (a+b));
+//	}
+//	public void add(float a, float b) {
+//		System.out.println("Adding (int, int):"+ (a+b));
+//	}
+//
+//}
+
+
+//public class Calculator {
+//	public void divide(int num1, int num2) {
+//		if(num2 == 0) {//declare my exception
+//			throw new ArithmeticException("Number cannot be divided by zero");
+//		}
+//		else {
+//			System.out.println("Div (int, int):"+ (num1/num2));
+//		}
+//	}
+//	
+//
+//}
+
+//public class Calculator {
+//	public void divide(int num1, int num2) throws ArithmeticException {
+//		System.out.println("Div (int, int):"+ (num1/num2));
+//	}
+//	
+//
+//}
+
+public class Calculator {
+	public void add(int a, int b) {
+		System.out.println("Adding (int, int):"+ (a+b));
+	}
+	public void add(int a, float b) {
+		System.out.println("Adding (int, int):"+ (a+b));
+	}
+	public void add(float a, int b) {
+		System.out.println("Adding (int, int):"+ (a+b));
+	}
+	public void add(float a, float b) {
+		System.out.println("Adding (int, int):"+ (a+b));
+	}
+
+}
