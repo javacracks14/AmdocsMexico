79fd3cbf8ed1431716fae8bf7a37751cd611cd82
Jean Carlos Carrazco Santos <jeancarrazco33@gmail.com>
2025-08-26 00:19:14 +0530

    Create DataTypesNVariablesDemo.java

---
+++ Jean Carrazco/DataTypesNVariablesDemo.java
@@ -0,0 +1,22 @@
+package com.amdocs;
+
+public class DataTypesNVariablesDemo {
+	public static void main(String[] args) {
+		int wholeNumber; //Declaring a variable
+		wholeNumber = 10; //Initialize variable
+		
+		float decimalNumber1 = 12.36f;
+		double decimalNumber2 = 199.123;
+		char singleAlphabet = 'a';
+		boolean value = true;
+		long longIntValue = 2352;
+		short shortIntVariable = 2324;
+		System.out.println("The value in Integer is "+wholeNumber);
+		System.out.println("The value in float is "+decimalNumber1);
+		System.out.println("The value in double is "+decimalNumber2);
+		System.out.println("The value in char is "+singleAlphabet+"\nThe value in boolean is "+value+"\n");
+		System.out.println("\"Hello\" from \"Amdocs\"");
+		System.out.println("The value in long is "+longIntValue+"\nThe value in short is "+shortIntVariable);
+	
+}
+}
