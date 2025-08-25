aeb790f0fd3686018d031f7f388dc52b8027965a
Jean Carlos Carrazco Santos <jeancarrazco33@gmail.com>
2025-08-26 00:18:41 +0530

    Create Circle.java

---
+++ Jean Carrazco/Circle.java
@@ -0,0 +1,30 @@
+package com.amdocs;
+
+//public class Circle implements Shape{
+//	@Override
+//	public void area() {
+//		System.out.println("Area of a circle");
+//	}
+//	@Override
+//	public void volume() {
+//		System.out.println("Volume of a circle");
+//	}
+//
+//}
+//public class Circle extends Shape{
+//	@Override
+//	public void area() {
+//		System.out.println("Area of a rectangle");
+//	}
+//}
+//public class Circle extends OperatorsDemo implements Shape, Demo {
+//	@Override
+//	public void area() {
+//		System.out.println("Area of a circle");
+//	}
+//	@Override
+//	public void volume() {
+//		System.out.println("Volume of a circle");
+//	}
+//
+//}
