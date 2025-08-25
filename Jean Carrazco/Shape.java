c50bb80558e0b9119376f7b4141976b2b1e5b489
Jean Carlos Carrazco Santos <jeancarrazco33@gmail.com>
2025-08-26 00:28:45 +0530

    Create Shape.java

---
+++ Jean Carrazco/Shape.java
@@ -0,0 +1,23 @@
+package com.amdocs;
+
+//public interface Shape {
+//	// the metods by default are public and abtract
+//	void area();
+//	void volume();
+//
+//}
+
+//public abstract class Shape{
+//	public void shapeType()//non abtract method
+//	{
+//		System.out.println("Structure of a shape");
+//	}
+//	public abstract void area();//abtract method
+//}
+public abstract class Shape{
+	public void shapeType()//non abtract method
+{
+		System.out.println("Structure of a shape");
+}
+	public abstract void area();//abtract method
+}
